199a636c1442b92c171c8bd4ccf10b1271043571
zXaViHz <144187401+zXaViHz@users.noreply.github.com>
2023-09-13 11:48:22 +0700

    Add files via upload

---
+++ Search_/src/common/Algorithm.java
@@ -0,0 +1,45 @@
+package common;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+abstract class Algorithm {
+    abstract int search(int[] arr, int target);
+}
+class LinearSearch extends Algorithm {
+    @Override
+    int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+class BinarySearch extends Algorithm {
+    @Override
+    int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+
---
+++ Search_/src/common/Library.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Common;
+
+import java.util.Random;
+import java.util.Scanner;
+public class Library {
+    
+}
